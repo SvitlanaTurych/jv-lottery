a546e0d430fd13c42fecbc8717e79b9ab50cc7bf
Svitlana Turych <svitlana.study24@gmail.com>
2025-11-21 16:08:00 +0200

    made correct order for variable defenition

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
     private final Random random;
-    private final int count = 100;
     private final ColorSupplier colorSupplier;
 
     public Lottery(Random rnd, ColorSupplier colorSupplier) {
@@ -13,7 +13,7 @@ public class Lottery {
     }
 
     public Ball getRandomBall() {
-        int randomNumber = random.nextInt(count) + 1;
+        int randomNumber = random.nextInt(MAX_NUMBER) + 1;
         Color color = colorSupplier.getRandomColor();
         return new Ball(color, randomNumber);
     }
