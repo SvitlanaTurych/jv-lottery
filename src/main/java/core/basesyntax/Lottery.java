56d157f6031eb5f834ab42923d6f54c9bbf66b0b
Svitlana Turych <svitlana.study24@gmail.com>
2025-11-21 15:50:18 +0200

    made correct order for variable defenition count

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private final Random random;
-    private static final int count = 100;
+    private final static int count = 100;
     private final ColorSupplier colorSupplier;
 
     public Lottery(Random rnd, ColorSupplier colorSupplier) {
